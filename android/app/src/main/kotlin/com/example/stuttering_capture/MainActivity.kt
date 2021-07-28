c370dd2bc94ebc9fba6a6ee6e07c969f0d990799
Nathan Millwater <millwater8919@gmail.com>
2021-07-28 14:49:23 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/stuttering_capture/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stuttering_capture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,170 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+Future<void> main() async {
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    print(e.toString());
+  }
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: SessionPage(title: 'Session View'),
+    );
+  }
+}
+
+class SessionPage extends StatefulWidget {
+  SessionPage({this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _SessionPageState createState() => _SessionPageState();
+}
+
+List<CameraDescription> cameras = [];
+
+class _SessionPageState extends State<SessionPage> {
+  int _counter = 0;
+  String sessionText = "He determined to drop his litigation with the monastry, and relinguish his claims to the wood-cuting and fishery rihgts at once. He was the more ready to do this becuase the rights had becom much less valuable, and he had indeed the vaguest idea where the wood and river in quedtion were. He determined to drop his litigation with the monastry, and relinguish his claims to the wood-cuting and fishery rihgts at once. He was the more ready to do this becuase the rights had becom much less valuable, and he had indeed the vaguest idea where the wood and river in quedtion were. He determined to drop his litigation with the monastry, and relinguish his claims to the wood-cuting and fishery rihgts at once. He was the more ready to do this becuase the rights had becom much less valuable, and he had indeed the vaguest idea where the wood and river in quedtion were. He determined to drop his litigation with the monastry, and relinguish his claims to the wood-cuting and fishery rihgts at once. He was the more ready to do this becuase the rights had becom much less valuable, and he had indeed the vaguest idea where the wood and river in quedtion were.";
+  CameraController controller;
+  String filePath;
+
+  _SessionPageState() {
+    setupCamera();
+  }
+
+  void setupCamera() {
+    controller = CameraController(
+      cameras[1],
+      ResolutionPreset.medium,
+      enableAudio: true);
+
+    try {
+      controller.initialize();
+    } on CameraException catch (e) {
+      print(e.toString());
+    }
+
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Column(
+        // Column is also a layout widget. It takes a list of children and
+        // arranges them vertically. By default, it sizes itself to fit its
+        // children horizontally, and tries to be as tall as its parent.
+        //
+        // Invoke "debug painting" (press "p" in the console, choose the
+        // "Toggle Debug Paint" action from the Flutter Inspector in Android
+        // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+        // to see the wireframe for each widget.
+        //
+        // Column has various properties to control how it sizes itself and
+        // how it positions its children. Here we use mainAxisAlignment to
+        // center the children vertically; the main axis here is the vertical
+        // axis because Columns are vertical (the cross axis would be
+        // horizontal).
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(25),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Text(
+                  sessionText,
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
+            )
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  //foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                ),
+                onPressed: () {
+                  if (!controller.value.isRecordingVideo)
+                    onStartPressed();
+                  else
+                    onStopPressed();
+                },
+                child: controller.value.isRecordingVideo ?
+                Text ("stop recording") : Text('Start Session'),
+              ),
+            ),
+          )
+
+        ],
+      ),
+    );
+  }
+
+  Future<void> onStartPressed() async {
+    // Creates the file path where the video is to be saved
+    final Directory extDir = await getApplicationDocumentsDirectory();
+    final String dirPath = '${extDir.path}/Movies/flutter_test';
+    await Directory(dirPath).create(recursive: true);
+    String date = DateTime.now().toIso8601String();
+    filePath = '$dirPath/$date.mp4';
+
+
+    await controller.startVideoRecording(filePath);
+    setState(() {});
+  }
+
+  Future<void> onStopPressed() async {
+    await controller.stopVideoRecording();
+    setState(() {});
+  }
+
+}
